8bdb1ad379d2273cc1fd6975dc7f2cde71e1c418
Subash Adhikari <me@subash.com.au>
2017-05-23 16:46:55 +1000

    add web methods in the soap interface

--- Todo-web/src/main/java/au/com/subash/soap/TodoListService.java
+++ Todo-web/src/main/java/au/com/subash/soap/TodoListService.java
@@ -1,13 +1,47 @@
 package au.com.subash.soap;
 
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
+import au.com.subash.entity.AppUser;
+import au.com.subash.entity.TodoItem;
+import au.com.subash.entity.TodoList;
+
 @WebService
 @SOAPBinding(style = Style.RPC)
 public interface TodoListService {
 
-  @WebMethod String sayHello(String name);
+  @WebMethod
+  AppUser getUser(int userId);
+
+  @WebMethod
+  void addTodoList(int userId, TodoList list);
+
+//   @WebMethod
+//   List<TodoList> getTodoLists(int userId);
+
+  @WebMethod
+  void deleteTodoList(int userId, int listId);
+
+  @WebMethod
+  void updateTodoList(int userId, TodoList list);
+
+  // @WebMethod
+  // List<TodoList> getTodoListItems(int userId, int listId);
+
+  @WebMethod
+  TodoItem addTodoItem(int userId, int listId, TodoItem item);
+
+  @WebMethod
+  TodoItem getTodoItem(int userId, int listId, int itemId);
+
+  @WebMethod
+  void deleteTodoItem(int userId, int listId, int itemId);
+
+  @WebMethod
+  void updateTodoItem(int userId, int listId, TodoItem item);
 }
--- Todo-web/src/main/java/au/com/subash/soap/TodoListServiceImpl.java
+++ Todo-web/src/main/java/au/com/subash/soap/TodoListServiceImpl.java
@@ -1,15 +1,77 @@
 package au.com.subash.soap;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.jws.WebService;
+
+import au.com.subash.entity.AppUser;
+import au.com.subash.entity.TodoItem;
+import au.com.subash.entity.TodoList;
+import au.com.subash.session.ManageTodoListFacadeRemote;
 
 @WebService(endpointInterface = "au.com.subash.soap.TodoListService")
 public class TodoListServiceImpl implements TodoListService {
-  private final String message = "Hello, ";
+
+  @EJB
+  private ManageTodoListFacadeRemote facade;
 
   public TodoListServiceImpl() { }
 
   @Override
-  public String sayHello(String name) {
-    return message + name + ".";
+  public AppUser getUser(int userId) {
+    return facade.getUser(userId);
   }
+
+  @Override
+  public void addTodoList(int userId, TodoList list) {
+
+  }
+
+  @Override
+  public void deleteTodoList(int userId, int listId) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void updateTodoList(int userId, TodoList list) {
+    // TODO Auto-generated method stub
+
+  }
+
+  // @Override
+  // public List<TodoList> getTodoListItems(int userId, int listId) {
+  //   // TODO Auto-generated method stub
+  //   return null;
+  // }
+
+  @Override
+  public TodoItem addTodoItem(int userId, int listId, TodoItem item) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public TodoItem getTodoItem(int userId, int listId, int itemId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void deleteTodoItem(int userId, int listId, int itemId) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void updateTodoItem(int userId, int listId, TodoItem item) {
+    // TODO Auto-generated method stub
+
+  }
+
+  //   @Override
+//   public List<TodoList> getTodoLists(int userId) {
+//     return null;
+//   }
 }
